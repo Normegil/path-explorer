5391c0b7b033eb4ff3f79dc231f47cbed8024604
Olivier Ory <mail@normegil.be>
2015-11-13 16:25:35 +0100

    Switch callback to Promises

--- README.md
+++ README.md
@@ -16,14 +16,14 @@ The library will send back an object with two methods (get & set):
 ```javascript
 var pathExplorer = require('path-explorer');
 var value = pathExplorer.get(object, 'w.x.array[1][2][3].z');
-var value = pathExplorer.set(
-  {
-    target: object,
-    path: 'a.b.c[1][2][3].d',
-    value: 'Example',
-  }, function onSet(err, result) {
-    // Result is your modified target but target will be modified as well
-  });
+var value = pathExplorer.set({
+  target: object,
+  path: 'a.b.c[1][2][3].d',
+  value: 'Example',
+}).then(function onSuccess(result) {
+  // Result is your modified target but the source target will be modified as well
+}).catch(function onError(err) {}); function onSet(err, result) {
+});
 ```
 
 ### Get
@@ -32,6 +32,6 @@ Synchronised. Relatively straightforward, it will send any value found following
 For getting values in array, you can use the standard array accessing notation of JS in the path (array[index1][index2][index3]). The dimensions are not limited. An index should always be present otherwise an error will be thrown.
 
 ### Set
-Asynchronized. Using the same path notation as for the getter, you can set a value in the object. Any object not existing in the path will be created, as well as arrays. Again, multi-dimension arrays are supported.
+Asynchronized (Using Promises). Using the same path notation as for the getter, you can set a value in the object. Any object not existing in the path will be created, as well as arrays. Again, multi-dimension arrays are supported.
 
 Concerning the array, if an array is specified with an empty index (`[]`) the setter will create a new element at the end of the array. If the index is larger than the array length, the element will still be created at the right index in the array, creating empty elements if necessary.
--- test/set.js
+++ test/set.js
@@ -6,134 +6,125 @@ var set = require('../lib/set.js');
 var moduleName = 'set() ';
 test(moduleName + 'should send an error if path is null', function(assert) {
   var target = {};
-  set(
-    {
-      target: target,
-      path: null,
-      value: 'test',
-    },
-    function(err) {
-      if (err) {return assert.end();}
-      return assert.fail(new Error('didn \'t send an error as expected'));
-    });
+  set({
+    target: target,
+    path: null,
+    value: 'test',
+  }).then(function onSuccess() {
+    assert.fail(new Error('didn \'t send an error as expected'));
+  }).catch(function onError() {
+    assert.end();
+  });
 });
 
 test(moduleName + 'should send an error if path is undefined', function(assert) {
   var target = {};
-  set(
-    {
+  set({
       target: target,
       value: 'test',
-    },
-    function(err) {
-      if (err) {return assert.end();}
-      return assert.fail(new Error('didn \'t send an error as expected'));
+    }).then(function onSuccess() {
+      assert.fail(new Error('didn \'t send an error as expected'));
+    }).catch(function onError() {
+      assert.end();
     });
 });
 
 test(moduleName + 'should not send an error if target is undefined', function(assert) {
   var target;
-  set(
-    {
+  set({
       target: target,
       path: '',
       value: 'test',
-    },
-    function(err, result) {
-      if (err) {return assert.fail(err);}
+    }).then(function onSuccess(result) {
       assert.equal(result, 'test');
-      return assert.end();
+      assert.end();
+    }).catch(function onError(err) {
+      assert.fail(err);
     });
 });
 
 test(moduleName + 'should not send an error if target is null', function(assert) {
   var target = null;
-  set(
-    {
-      target: target,
-      path: '',
-      value: 'test',
-    },
-    function(err, result) {
-      if (err) {return assert.fail(err);}
-      assert.equal(result, 'test');
-      return assert.end();
-    });
+  set({
+    target: target,
+    path: '',
+    value: 'test',
+  }).then(function onSuccess(result) {
+    assert.equal(result, 'test');
+    assert.end();
+  }).catch(function onError(err) {
+    assert.fail(err);
+  });
 });
 
 test(moduleName + 'should set root value', function(assert) {
   var target = {};
-  set(
-    {
-      target: target,
-      path: '',
-      value: 'test',
-    },
-    function(err, result) {
-      if (err) {return assert.end();}
-      assert.equal(result, 'test');
-      return assert.end();
-    });
+  set({
+    target: target,
+    path: '',
+    value: 'test',
+  }).then(function onSuccess(result) {
+    assert.equal(result, 'test');
+    assert.end();
+  }).catch(function onError(err) {
+    assert.fail(err);
+  });
 });
 
 test(moduleName + 'should set root value of undefined target', function(assert) {
   var target;
-  set(
-    {
-      target: target,
-      path: 'testField',
-      value: 'test',
-    },
-    function(err, result) {
-      if (err) {return assert.end();}
-      assert.equal(result.testField, 'test');
-      return assert.end();
-    });
+  set({
+    target: target,
+    path: 'testField',
+    value: 'test',
+  }).then(function onSuccess(result) {
+    assert.equal(result.testField, 'test');
+    assert.end();
+  }).catch(function onError(err) {
+    assert.fail(err);
+  });
 });
 
 test(moduleName + 'should set root value of null target', function(assert) {
   var target = null;
-  set(
-    {
-      target: target,
-      path: 'testField',
-      value: 'test',
-    },
-    function(err, result) {
-      if (err) {return assert.end();}
-      assert.equal(result.testField, 'test');
-      return assert.end();
-    });
+  set({
+    target: target,
+    path: 'testField',
+    value: 'test',
+  }).then(function onSuccess(result) {
+    assert.equal(result.testField, 'test');
+    assert.end();
+  }).catch(function onError(err) {
+    assert.fail(err);
+  });
 });
 
 test(moduleName + 'should set property value', function(assert) {
   var target = {};
-  set(
-    {
-      target: target,
-      path: 'testProperty',
-      value: 'test',
-    },
-    function(err) {
-      if (err) {return assert.end();}
-      assert.equal(target.testProperty, 'test');
-      return assert.end();
-    });
+  set({
+    target: target,
+    path: 'testProperty',
+    value: 'test',
+  }).then(function onSuccess() {
+    assert.equal(target.testProperty, 'test');
+    assert.end();
+  }).catch(function onError(err) {
+    assert.fail(err);
+  });
 });
 
 test(moduleName + 'should set property value in hierarchy', function(assert) {
   var target = {object: {}};
-  set(
-    {
-      target: target,
-      path: 'object.testProperty',
-      value: 'test',
-    },
-    function(err) {
-      if (err) {return assert.end();}
-      assert.equal(target.object.testProperty, 'test');
-      return assert.end();
-    });
+  set({
+    target: target,
+    path: 'object.testProperty',
+    value: 'test',
+  }).then(function onSuccess() {
+    assert.equal(target.object.testProperty, 'test');
+    assert.end();
+  }).catch(function onError(err) {
+    assert.fail(err);
+  });
 });
 
 test(moduleName + 'should not change other values', function(assert) {
@@ -141,183 +132,165 @@ test(moduleName + 'should not change other values', function(assert) {
     test1: 'test1',
     test2: 'test2',
   };
-  set(
-    {
-      target: target,
-      path: 'test1',
-      value: 'test',
-    },
-    function(err) {
-      if (err) {return assert.end();}
-      assert.equal(target.test1, 'test');
-      assert.equal(target.test2, 'test2');
-      return assert.end();
-    });
+  set({
+    target: target,
+    path: 'test1',
+    value: 'test',
+  }).then(function onSuccess() {
+    assert.equal(target.test1, 'test');
+    assert.equal(target.test2, 'test2');
+    assert.end();
+  }).catch(function onError(err) {
+    assert.fail(err);
+  });
 });
 
 test(moduleName + 'should set property value in non-existing hierarchy', function(assert) {
   var target = {};
-  set(
-    {
-      target: target,
-      path: 'object.testProperty',
-      value: 'test',
-    },
-    function(err) {
-      if (err) {return assert.end();}
-      assert.equal(target.object.testProperty, 'test');
-      return assert.end();
-    });
+  set({
+    target: target,
+    path: 'object.testProperty',
+    value: 'test',
+  }).then(function onSuccess() {
+    assert.equal(target.object.testProperty, 'test');
+    assert.end();
+  }).catch(function onError(err) {
+    assert.fail(err);
+  });
 });
 
 test(moduleName + 'should set array as property value', function(assert) {
   var target = {};
   var value = [1, 2, 3];
-  set(
-    {
-      target: target,
-      path: 'array',
-      value: value,
-    },
-    function(err) {
-      if (err) {return assert.fail(err);}
-      assert.equal(target.array, value);
-      return assert.end();
-    });
+  set({
+    target: target,
+    path: 'array',
+    value: value,
+  }).then(function onSuccess() {
+    assert.equal(target.array, value);
+    assert.end();
+  }).catch(function onError(err) {
+    assert.fail(err);
+  });
 });
 
 test(moduleName + 'should set value for composed property name', function(assert) {
   var target = {};
   var value = 'Test';
   var propertyName = 'Test Object';
-  set(
-    {
-      target: target,
-      path: propertyName,
-      value: value,
-    },
-    function(err) {
-      if (err) {return assert.fail(err);}
-      assert.equal(target[propertyName], value);
-      return assert.end();
-    });
+  set({
+    target: target,
+    path: propertyName,
+    value: value,
+  }).then(function onSuccess() {
+    assert.equal(target[propertyName], value);
+    assert.end();
+  }).catch(function onError(err) {
+    assert.fail(err);
+  });
 });
 
 test(moduleName + 'should replace internal value of an array', function(assert) {
   var target = {array: [1 ,2 ,3]};
   var value = 4;
-  set(
-    {
-      target: target,
-      path: 'array[1]',
-      value: value,
-    },
-    function(err) {
-      if (err) {return assert.fail(err);}
-      assert.equal(target.array[1], value);
-      return assert.end();
-    }
-  );
+  set({
+    target: target,
+    path: 'array[1]',
+    value: value,
+  }).then(function onSuccess() {
+    assert.equal(target.array[1], value);
+    assert.end();
+  }).catch(function onError(err) {
+    assert.fail(err);
+  });
 });
 
 test(moduleName + 'should be able to set an internal array value', function(assert) {
   var target = {array: [1 ,2 ,3]};
   var value = 4;
-  set(
-    {
-      target: target,
-      path: 'array[1].test',
-      value: value,
-    },
-    function(err) {
-      if (err) {return assert.fail(err);}
-      assert.equal(target.array[1].test, value);
-      return assert.end();
-    }
-  );
+  set({
+    target: target,
+    path: 'array[1].test',
+    value: value,
+  }).then(function onSuccess() {
+    assert.equal(target.array[1].test, value);
+    assert.end();
+  }).catch(function onError(err) {
+    assert.fail(err);
+  });
 });
 
 test(moduleName + 'should send an error when setting an internal array value if array index doesn\'t exist', function(assert) {
   var target = {array: [1 ,2 ,3]};
   var value = 4;
-  set(
-    {
-      target: target,
-      path: 'array[42].test',
-      value: value,
-    },
-    function(err) {
-      if (err) {return assert.fail(err);}
-      assert.equal(value, target.array[42].test);
-      assert.end();
-    }
-  );
+  set({
+    target: target,
+    path: 'array[42].test',
+    value: value,
+  }).then(function onSuccess() {
+    assert.equal(value, target.array[42].test);
+    assert.end();
+  }).catch(function onError(err) {
+    assert.fail(err);
+  });
 });
 
 test(moduleName + 'should be able to add an internal array value if no index is specified', function(assert) {
   var target = {array: [1 ,2 ,3]};
   var value = 4;
-  set(
-    {
-      target: target,
-      path: 'array[].test',
-      value: value,
-    },
-    function(err) {
-      if (err) {return assert.fail(err);}
-      assert.equal(target.array[3].test, value);
-      return assert.end();
-    }
-  );
+  set({
+    target: target,
+    path: 'array[].test',
+    value: value,
+  }).then(function onSuccess() {
+    assert.equal(target.array[3].test, value);
+    assert.end();
+  }).catch(function onError(err) {
+    assert.fail(err);
+  });
 });
 
 test(moduleName + 'should be able to set an internal array value if array index exist on multiple dimension array', function(assert) {
   var target = {array: [[1, 2, 3], [21, 22, 23], [31, 32, 33]]};
   var value = 4;
-  set(
-    {
-      target: target,
-      path: 'array[1][2].test',
-      value: value,
-    },
-    function(err) {
-      if (err) {return assert.fail(err);}
-      assert.equal(target.array[1][2].test, value);
-      return assert.end();
-    }
-  );
+  set({
+    target: target,
+    path: 'array[1][2].test',
+    value: value,
+  }).then(function onSuccess() {
+    assert.equal(target.array[1][2].test, value);
+    assert.end();
+  }).catch(function onError(err) {
+    assert.fail(err);
+  });
 });
 
 test(moduleName + 'should be able to replace base property with an array', function(assert) {
   var target = {array: 'Test'};
   var value = 4;
-  set(
-    {
-      target: target,
-      path: 'array[1].test',
-      value: value,
-    },
-    function(err) {
-      if (err) {return assert.fail(err);}
-      assert.equal(value, target.array[1].test);
-      return assert.end();
-    }
-  );
+  set({
+    target: target,
+    path: 'array[1].test',
+    value: value,
+  }).then(function onSuccess() {
+    assert.equal(value, target.array[1].test);
+    assert.end();
+  }).catch(function onError(err) {
+    assert.fail(err);
+  });
 });
 
 test(moduleName + 'should be able to set an internal array to replace actual value', function(assert) {
   var target = {array: [1, 2, 3]};
   var value = 4;
-  set(
-    {
-      target: target,
-      path: 'array[1][2].test',
-      value: value,
-    },
-    function(err) {
-      if (err) {return assert.fail(err);}
-      assert.equal(value, target.array[1][2].test);
-      return assert.end();
-    }
-  );
+  set({
+    target: target,
+    path: 'array[1][2].test',
+    value: value,
+  }).then(function onSuccess() {
+    assert.equal(value, target.array[1][2].test);
+    assert.end();
+  }).catch(function onError(err) {
+    assert.fail(err);
+  });
 });
